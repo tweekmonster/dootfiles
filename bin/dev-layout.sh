564b121a30d972edd2c31924ee96060648de9728
Tommy Allen <tommy@esdf.io>
2016-07-02 12:54:32 -0400

    Don't change tmux window title

--- bin/dev-layout.sh
+++ bin/dev-layout.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 panes=$(tmux list-panes | wc -l)
 venv=${VIRTUAL_ENV##*/}
-name=${venv:-$(tmux display -p "#W")}
-name=${1:-$name}
 size=${2:-100}
-
-tmux rename-window $name
 
 if [[ $panes -ge 4 ]]; then
   tmux resize-pane -x $size -t 2
