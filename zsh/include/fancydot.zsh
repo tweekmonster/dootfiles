23faf0c8a553cccfdc78d0d0ed96c4d116495fd8
Tommy Allen <tommy@esdf.io>
2016-07-30 11:56:00 -0400

    Fix fancy-dot "bad pattern"

--- zsh/include/fancydot.zsh
+++ zsh/include/fancydot.zsh
@@ -1,7 +1,8 @@
 # Thanks @mhinz
 # https://github.com/mhinz/dotfiles/blob/fe54d9fec9915115355835de22556e38ac7a8b17/zsh/.zshrc#L308-L326
 fancy-dot() {
-    local -a split=( ${=LBUFFER} )
+    local -a split
+    split=( ${=LBUFFER} )
     local dir=$split[-1]
     if [[ $LBUFFER =~ '(^| )(\.\./)+$' ]]; then
         zle self-insert
