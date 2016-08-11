7410a0af0f5a9f56e2c5ab6f4a3f141e90921f51
Tommy Allen <tommy@esdf.io>
2016-08-11 11:48:30 -0400

    Made gen-terminfo.sh a little more readable.

--- scripts/gen-terminfo.sh
+++ scripts/gen-terminfo.sh
@@ -20,7 +20,9 @@ generate() {
   echo "Generating $1"
   tmpfile=$(mktemp /tmp/terminfo.XXXXXXX)
   /usr/bin/infocmp "$1" | sed -e 's/\(ritm\|sitm\|rmso\|smso\)=[^,]\+, *//g' > $tmpfile
-  echo '\tritm=\E[23m, rmso=\E[27m, sitm=\E[3m, smso=\E[7m, ms@,' >> $tmpfile
+  echo '	sitm=\E[3m, ritm=\E[23m,' >> $tmpfile
+  echo '	smso=\E[7m, rmso=\E[27m,' >> $tmpfile
+  echo '	Ms@,' >> $tmpfile
   /usr/bin/tic -x $tmpfile
   rm -f $tmpfile
 }
